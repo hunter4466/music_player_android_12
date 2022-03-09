9e6c1b21304343b4b02490b6aa37872ac6785379
Mario Chois <mario.chois.navarro@gmail.com>
2022-03-09 01:13:49 -0500

    ⚡️Deploy: Setup booilerplate application with defined layers

---
+++ app/src/main/java/com/ravnnerdery/music_player_android_12/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ravnnerdery.music_player_android_12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
